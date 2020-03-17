354afbffce99590c03390b3c2d1b25d0f5c6d324
master <58977579+alexmasterblack@users.noreply.github.com>
2020-03-17 14:24:57 +1000

    Update bucket_sort.py

--- bucket_sort.py
+++ bucket_sort.py
@@ -4,23 +4,26 @@ def merge_mass(mass):
       new_mass.extend(i)
     return new_mass
 
+def join_mass(mass):
+    new_mass = []
+    for i in range(0, len(mass) - 2, 3):
+        new_mass.append([mass[i], mass[i + 1], mass[i + 2]])
+    return new_mass
+    
+
 def bucket_sort(mass_words, n):
     if n < 0:
         return mass_words
     else:
-        support_mass = [[] for i in range(0, 256)]
         
+        support_mass = [[] for i in range(123)]
         for i in range(len(mass_words)):
             letter = mass_words[i]
-            support_mass[ord(letter[n])].extend(letter)
-
+            support_mass[ord(letter[n])].append(letter)
+        
         sorted_mass = merge_mass([i for i in support_mass if i != []])
         
-        new_sorted_mass = []
-        for i in range(0, len(sorted_mass) - 2, 3):
-            new_sorted_mass.append([sorted_mass[i], sorted_mass[i + 1], sorted_mass[i + 2]])
-        
-        return bucket_sort(new_sorted_mass, n - 1)
+        return bucket_sort(sorted_mass, n - 1)
 
 fin = open ('input.txt', 'r')            
 fout = open ('output.txt', 'w')
@@ -28,10 +31,14 @@ fout = open ('output.txt', 'w')
 string = fin.read().splitlines()
 del string[0]
 
+new_string = []
+for i in string:
+    new_string.append([j for j in i])
+
 sorted_mass = []
-for i in bucket_sort(string, 2):
+for i in bucket_sort(new_string, 2):
     for j in i:
         sorted_mass.append(j)
-        
-for i in range(0, len(sorted_mass) - 2, 3):
-    fout.write(sorted_mass[i] + sorted_mass[i + 1] + sorted_mass[i + 2] + '\n')
+
+for i in join_mass(sorted_mass):
+    fout.write(''.join(i) + '\n')
