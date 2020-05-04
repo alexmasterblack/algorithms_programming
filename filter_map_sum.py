0a80f5c7d2ccd45d54a0266ecca5666e428be444
master <58977579+alexmasterblack@users.noreply.github.com>
2020-05-05 02:39:57 +1000

    Delete filter_map_sum.py

--- filter_map_sum.py
+++
@@ -1,9 +0,0 @@
-def check(i):
-    if i % 9 == 0:
-        return 1
-    else:
-        return 0
-
-print(sum(map(lambda i: i**2, filter(check, range(10,100)))))
-
-print(sum(map(lambda i: i**2, filter(lambda i: i % 9 == 0, range(10,100)))))
